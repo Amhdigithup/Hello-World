7be3bda09cd06c3bf16df4294d8d2b64a23c22f3
ShulkwiSEC <shulkwisec@gmail.com>
2024-10-01 20:30:54 +0300

    update hello_world style < app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 # im searching for some one to adjest my code and conteribe with me to init this repo
+import termcolor
+import random
 
-print('Hello World')
+def log(d):
+    datas = d.split(' ')
+    colors = list(termcolor.COLORS.keys())
+    fainltext = ''
+    for d in datas:
+        fainltext += termcolor.colored(d,random.choice(colors))
+    print(fainltext)
+
+msg = ' Hello World'
+for i in range(10):
+    log(msg * i)
